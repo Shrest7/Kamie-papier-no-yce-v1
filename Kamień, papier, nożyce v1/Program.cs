480bbfd1c587025741b64c9cff37033b0fb588c2
Shrest7 <dravish@wp.pl>
2017-09-04 21:12:40 +0200

    added

--- Kamień, papier, nożyce v1/Program.cs
+++ Kamień, papier, nożyce v1/Program.cs
@@ -8,13 +8,14 @@ namespace Kamień__papier__nożyce_v1
 {
     class Program
     {
+        static int wygrana;
+        static int remis;
+        static int przegrana;
+
         static void Main(string[] args)
         {
-            Random liczbaLosowa = new Random();
-            int wygrana = 0;
-            int remis = 0;
-            int przegrana = 0;
             char gramy = 't';
+            Random liczbaLosowa = new Random();
             while (gramy == 't')
             {
                 int liczbaPrzeciwnika = liczbaLosowa.Next(1, 4);
@@ -24,78 +25,9 @@ namespace Kamień__papier__nożyce_v1
                 Console.WriteLine("3. Nożyce");
                 int wybor = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("");
-                if (wybor == 1 && liczbaPrzeciwnika == 1)
-                {
-                    Console.WriteLine("Pokazałeś kamień");
-                    Console.WriteLine("Przeciwnik pokazał kamień");
-                    Console.WriteLine("Remis!");
-                    remis++;
-                }
+                funkcja(wybor, liczbaPrzeciwnika);
 
-                if (wybor == 1 && liczbaPrzeciwnika == 2)
-                {
-                    Console.WriteLine("Pokazałeś kamień");
-                    Console.WriteLine("Przeciwnik pokazał papier");
-                    Console.WriteLine("Przegrałeś.");
-                    przegrana++;
-                }
-
-                if (wybor == 1 && liczbaPrzeciwnika == 3)
-                {
-                    Console.WriteLine("Pokazałeś kamień");
-                    Console.WriteLine("Przeciwnik pokazał nożyce");
-                    Console.WriteLine("Wygrałeś!");
-                    wygrana++;
-                }
-
-                if (wybor == 2 && liczbaPrzeciwnika == 1)
-                {
-                    Console.WriteLine("Pokazałeś papier");
-                    Console.WriteLine("Przeciwnik pokazał kamień");
-                    Console.WriteLine("Wygrałeś!");
-                    wygrana++;
-                }
-
-                if (wybor == 2 && liczbaPrzeciwnika == 2)
-                {
-                    Console.WriteLine("Pokazałeś papier");
-                    Console.WriteLine("Przeciwnik pokazał papier");
-                    Console.WriteLine("Remis!");
-                    remis++;
-                }
-
-                if (wybor == 2 && liczbaPrzeciwnika == 3)
-                {
-                    Console.WriteLine("Pokazałeś papier");
-                    Console.WriteLine("Przeciwnik pokazał nożyce");
-                    Console.WriteLine("Przegrałeś.");
-                    przegrana++;
-                }
-
-                if (wybor == 3 && liczbaPrzeciwnika == 1)
-                {
-                    Console.WriteLine("Pokazałeś nożyce");
-                    Console.WriteLine("Przeciwnik pokazał kamień");
-                    Console.WriteLine("Przegrałeś.");
-                    przegrana++;
-                }
-
-                if (wybor == 3 && liczbaPrzeciwnika == 2)
-                {
-                    Console.WriteLine("Pokazałeś nożyce");
-                    Console.WriteLine("Przeciwnik pokazał papier");
-                    Console.WriteLine("Wygrałeś!");
-                    wygrana++;
-                }
-
-                if (wybor == 3 && liczbaPrzeciwnika == 3)
-                {
-                    Console.WriteLine("Pokazałeś nożyce");
-                    Console.WriteLine("Przeciwnik pokazał nożyce");
-                    Console.WriteLine("Remis!");
-                    remis++;
-                }
+                Console.WriteLine("");                
                 Console.WriteLine("Gramy dalej? (t/n)");
                 Console.WriteLine("");
                 gramy = char.Parse(Console.ReadLine());
@@ -104,6 +36,81 @@ namespace Kamień__papier__nożyce_v1
             Console.WriteLine("Remisy: {0}", remis);
             Console.WriteLine("Przegrane: {0}", przegrana);
             Console.ReadKey();
+        }
+
+        static void funkcja(int wybor, int liczbaPrzeciwnika)
+        {
+            if (wybor == 1 && liczbaPrzeciwnika == 1)
+            {
+                Console.WriteLine("Pokazałeś kamień");
+                Console.WriteLine("Przeciwnik pokazał kamień");
+                Console.WriteLine("Remis!");
+                remis++;
+            }
+
+            if (wybor == 1 && liczbaPrzeciwnika == 2)
+            {
+                Console.WriteLine("Pokazałeś kamień");
+                Console.WriteLine("Przeciwnik pokazał papier");
+                Console.WriteLine("Przegrałeś.");
+                przegrana++;
+            }
+
+            if (wybor == 1 && liczbaPrzeciwnika == 3)
+            {
+                Console.WriteLine("Pokazałeś kamień");
+                Console.WriteLine("Przeciwnik pokazał nożyce");
+                Console.WriteLine("Wygrałeś!");
+                wygrana++;
+            }
+
+            if (wybor == 2 && liczbaPrzeciwnika == 1)
+            {
+                Console.WriteLine("Pokazałeś papier");
+                Console.WriteLine("Przeciwnik pokazał kamień");
+                Console.WriteLine("Wygrałeś!");
+                wygrana++;
+            }
+
+            if (wybor == 2 && liczbaPrzeciwnika == 2)
+            {
+                Console.WriteLine("Pokazałeś papier");
+                Console.WriteLine("Przeciwnik pokazał papier");
+                Console.WriteLine("Remis!");
+                remis++;
+            }
+
+            if (wybor == 2 && liczbaPrzeciwnika == 3)
+            {
+                Console.WriteLine("Pokazałeś papier");
+                Console.WriteLine("Przeciwnik pokazał nożyce");
+                Console.WriteLine("Przegrałeś.");
+                przegrana++;
+            }
+
+            if (wybor == 3 && liczbaPrzeciwnika == 1)
+            {
+                Console.WriteLine("Pokazałeś nożyce");
+                Console.WriteLine("Przeciwnik pokazał kamień");
+                Console.WriteLine("Przegrałeś.");
+                przegrana++;
+            }
+
+            if (wybor == 3 && liczbaPrzeciwnika == 2)
+            {
+                Console.WriteLine("Pokazałeś nożyce");
+                Console.WriteLine("Przeciwnik pokazał papier");
+                Console.WriteLine("Wygrałeś!");
+                wygrana++;
+            }
+
+            if (wybor == 3 && liczbaPrzeciwnika == 3)
+            {
+                Console.WriteLine("Pokazałeś nożyce");
+                Console.WriteLine("Przeciwnik pokazał nożyce");
+                Console.WriteLine("Remis!");
+                remis++;
+            }
         }
     }
 }
